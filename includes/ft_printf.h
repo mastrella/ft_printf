398b92f92a8c243142ac3aced36ff8420e863f29
mastrella <michael_mastrella@hotmail.com>
2017-04-13 18:34:50 -0800

    adding colours

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -94,4 +94,24 @@ char	*ft_strcpy(char *dst, const char *src);
 void	*ft_memalloc(size_t size);
 void	*ft_memset(void *b, int c, size_t len);
 
+/*  setting colours  */
+# define NORMAL    "\033[0m"
+# define LIGHT     "\033[1m"
+# define BLACK     "\033[30m"
+# define RED       "\033[31m"
+# define GREEN     "\033[32m"
+# define YELLOW    "\033[33m"
+# define BLUE      "\033[34m"
+# define MAGENTA   "\033[35m"
+# define CYAN      "\033[36m"
+# define WHITE     "\033[37m"
+# define BG_BLACK  "\033[40m"
+# define BG_RED    "\033[41m"
+# define BG_GREEN  "\033[42m"
+# define BG_YELLOW "\033[43m"
+# define BG_BLUE   "\033[44m"
+# define BG_VIOLET "\033[45m"
+# define BG_CYAN   "\033[46m"
+# define BG_WHITE  "\033[47m"
+
 #endif
